0d454b0eb3d079889a51be99aa3d94e8de299860
Shivam Srivastava <shivam.svt3012@gmail.com>
2025-07-08 22:22:06 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-green-500">
+      <h1 className='text-white content-center'>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
